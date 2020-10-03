7027f798fbd2a4ee6cb91794bf74702c314d3bce
Christina Wang <cywang117@outlook.com>
2020-10-03 17:50:29 +0900

    Complete load testing

--- server/index.js
+++ server/index.js
@@ -1,10 +1,6 @@
-const path = require("path");
-require("dotenv").config({
-  path:  path.resolve(__dirname, "../", ".env")
-});
+require("dotenv").config();
 const server = require("./server");
-const PORT = process.env.PORT;
 
-server.listen(PORT, () => {
-  console.log("Listening on port " + PORT);
+server.listen(process.env.PORT || 3000, () => {
+  console.log(`Listening on port ${process.env.PORT || 3000}`);
 });
--- server/server.js
+++ server/server.js
@@ -1,7 +1,5 @@
 const express = require("express");
-const morgan = require("morgan");
 const helmet = require("helmet");
-const request = require("superagent");
 const cors = require("cors");
 const pug = require("pug");
 const fetch = require("node-fetch");
@@ -10,9 +8,6 @@ const path = require("path");
 const app = express();
 
 app.set("view engine", "pug");
-
-app.use(morgan("dev"));
-//app.use(helmet());
 
 app.use(helmet.noSniff());
 app.use(
@@ -27,18 +22,39 @@ app.use(
 app.use("/static", express.static(path.resolve(__dirname, "../assets")));
 app.set("views", path.resolve(__dirname, "../views"));
 
-app.get("/:product_id", async (req, res) => {
+app.get("/:product_id", (req, res) => {
   res.status(200).render("index", {
-    descService: process.env.DESC_SERVICE,
-    overviewService: process.env.OVERVIEW_SERVICE,
-    traitsService: process.env.TRAITS_SERVICE,
-    imageService: process.env.IMAGE_SERVICE,
-    priceService: process.env.PRICE_SERVICE,
-    relatedService: process.env.RELATED_SERVICE,
-    otherPopService: process.env.OTHER_POP_SERVICE,
-    legalService: process.env.LEGAL_SERVICE,
+    descService: `${process.env.DESC_SERVICE}/bundle.js`,
+    // overviewService: process.env.OVERVIEW_SERVICE,
+    // traitsService: process.env.TRAITS_SERVICE,
+    // imageService: process.env.IMAGE_SERVICE,
+    // priceService: process.env.PRICE_SERVICE,
+    // relatedService: process.env.RELATED_SERVICE,
+    // otherPopService: process.env.OTHER_POP_SERVICE,
+    // legalService: process.env.LEGAL_SERVICE,
   });
 });
+
+/**
+ * API REROUTING
+ */
+// Description service
+app.get(['/description/title/:product_id', '/description/:product_id', '/genre/:product_id'], async (req, res) => {
+  let product_id = parseInt(req.params.product_id);
+  if (Number.isNaN(product_id) || product_id < 1) {
+    return res.status(400).send('Invalid product ID.');
+  }
+
+  try {
+    let result = await fetch(`${process.env.DESC_SERVICE}${req.path}`)
+      .then(rsp => req.path.includes('title') ? rsp.text() : rsp.json());
+    res.status(200).json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('An internal error occurred.');
+  }
+});
+
 
 app.get("/", (req, res) => {
   res.redirect("/21");
